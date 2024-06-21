5909a832058e0398541d59e7982012fb5a9554a5
Minjana <minjanaruwanpura@gmail.com>
2024-06-21 21:55:41 +0530

    Created Study Session Class

---
+++ StudentsPerformancePredictionTool-CW1/Models/StudySession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsPerformancePredictionTool_CW1.Models
+{
+    public class StudySession
+    {
+        public DateTime Date { get; set; }
+        public double Hours { get; set; }
+        public string Subject { get; set; }
+
+        public StudySession(DateTime date, double hours, string subject)
+        {
+            Date = date;
+            Hours = hours;
+            Subject = subject;
+        }
+
+    }
+}
