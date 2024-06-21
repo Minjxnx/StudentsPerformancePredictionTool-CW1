5574a8ca43800640e6db534cb7f9ec8549bb2241
Minjana <minjanaruwanpura@gmail.com>
2024-06-21 22:01:42 +0530

    Created the Break Session Class

---
+++ StudentsPerformancePredictionTool-CW1/Models/BreakSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsPerformancePredictionTool_CW1.Models
+{
+    public class BreakSession
+    {
+        public DateTime Date { get; set; }
+        public double Hours { get; set; }
+
+        public BreakSession(DateTime date, double hours)
+        {
+            Date = date;
+            Hours = hours;
+        }
+    }
+}
